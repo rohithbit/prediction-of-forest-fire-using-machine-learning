9d4faef0bb8df1813552b5493bb4683a6cb970cb
rohithbit <147087245+rohithbit@users.noreply.github.com>
2026-06-25 16:13:32 +0530

    Update practice.cs

--- practice.cs
+++ practice.cs
@@ -1,163 +1,104 @@
-ocelot.json
-Sumit Mitra(IN8230)
-25-06-2026 09:26
-{
-    "Routes": [
-        {
-        "DownstreamPathTemplate" : "/api/order",
-        "DownstreamScheme" : "http",
-        "DownstreamHostAndPorts" : [{
-            "Host" : "localhost",
-            "Port" : 8080
-        }
-    ],
-    "UpstreamPathTemplate" : "/order-api/order",
-    "UpstreamHttpMethod" : ["GET", "POST"]
-    }
-],
-"GlobalConfiguration": {}
-}
-Sumit Mitra(IN8230)
-25-06-2026 09:27
-apigateway-program.cs
-Sumit Mitra(IN8230)
-25-06-2026 09:27
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
- 
- 
-var builder = WebApplication.CreateBuilder(args);
- 
-// builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-//     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-//     .AddEnvironmentVariables();
- 
-builder.Configuration
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
- 
-builder.Services.AddControllers();
-builder.Services.AddOcelot();
- 
-var app = builder.Build();
- 
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseDeveloperExceptionPage();
-// }
- 
-// app.UseHttpsRedirection();
-// app.UseRouting();
-// app.UseAuthorization();
- 
-// app.MapControllers();
-await app.UseOcelot();
- 
-app.Run();
- 
-Sumit Mitra(IN8230)
-25-06-2026 09:27
-ordercontroller
-Sumit Mitra(IN8230)
-25-06-2026 09:27
+ProductController.cs
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnetapp.Data;
+using dotnetapp.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using OrderService.Data;
-using OrderService.Models;
  
-namespace OrderService.Controllers
+namespace dotnetapp.Controllers
 {
     [ApiController]
-    [Route("api/order")]
-    public class OrderController : Controller
+    [Route("api/products")]
+    public class ProductController : ControllerBase
     {
-        private readonly OrderDbContext db;
-        public OrderController(OrderDbContext db1){
-            db = db1;
+        public ApplicationDbContext db;
+        public ProductController(ApplicationDbContext db)
+        {
+            this.db=db;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Order>>> GetOrders(){
-            return await db.Orders.ToListAsync();
+        public ActionResult<IEnumerable<Product>> GetProducts(){
+            var prods=db.Products.ToList();
+            return Ok(prods);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Product>GetProductById(int id){
+        Product pro=db.Products.FirstOrDefault(p=>p.Id==id);
+        if(pro==null)return NotFound();
+        return Ok(pro);
+        }
+        [HttpGet("filter")]
+        public ActionResult<IEnumerable<Product>>GetProductsByCategory([FromQuery] string category){
+            List<Product>prods=db.Products.Where(p=>p.Category==category).ToList();
+            if(prods==null){
+                return NotFound();
+            }
+            return Ok(prods);
         }
         [HttpPost]
-        public async Task<ActionResult<Order>> CreateOrder(Order order){
-            db.Orders.Add(order);
-            await db.SaveChangesAsync();
-            return StatusCode(201, new{
-                message = "Order created successfully",
-                order
-            });
+        public ActionResult<Product>CreateProduct(Product product){
+             
+             db.Products.Add(product);
+             db.SaveChanges();
+            return CreatedAtAction("CreateProduct",product);
+ 
         }
     }
 }
-Sumit Mitra(IN8230)
-25-06-2026 09:28
-datafolder/appdbcontext
-Sumit Mitra(IN8230)
-25-06-2026 09:28
+ 
+Data/ApplicationDbContext.cs
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using OrderService.Models;
+using dotnetapp.Models;
 using Microsoft.EntityFrameworkCore;
- 
-namespace OrderService.Data
+namespace dotnetapp.Data
 {
-    public class OrderDbContext : DbContext
+    public class ApplicationDbContext :DbContext
     {
-        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options){
+       
+        public DbSet<Product>Products{get;set;}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options){
+           
         }
-        public DbSet<Order> Orders{get;set;}
     }
 }
-Sumit Mitra(IN8230)
-25-06-2026 09:28
+ 
+Models/Product.cs
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
- 
-namespace OrderService.Models
+using System.ComponentModel.DataAnnotations;
+namespace dotnetapp.Models
 {
-    public class Order
+    public class Product
     {
-        public int OrderId{get;set;}
-        public string CustomerName{get;set;}
-        public DateTime OrderDate {get;set;}
-        public decimal TotalAmount {get;set;}
+        [Key]
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public string Category{get;set;}
+        public decimal Price{get;set;}
+        public int Stock{get;set;}
+ 
     }
 }
-Sumit Mitra(IN8230)
-25-06-2026 09:28
-program.cs order
-Sumit Mitra(IN8230)
-25-06-2026 09:28
-using System.Buffers;
+ 
+Program.cs
+using dotnetapp.Data;
 using Microsoft.EntityFrameworkCore;
-using OrderService.Data;
 var builder = WebApplication.CreateBuilder(args);
  
 // Add services to the container.
-var con = "User ID=sa;password=examlyMssql@123;server=localhost;Database=appdb;trusted_connection=false;Persist Security Info=False;Encrypt=False";
+var conn="User ID=sa;password=examlyMssql@123;server=localhost;Database=appdb;trusted_connection=false;Persist Security Info=False;Encrypt=False";
 builder.Services.AddControllers();
-builder.Services.AddDbContext<OrderDbContext>(options => options.UseSqlServer(con));
- 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at Get started with Swashbuckle and ASP.NET Core | Microsoft Learn
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
- 
+builder.Services.AddDbContext<ApplicationDbContext>(app=>app.UseSqlServer(conn));
 var app = builder.Build();
  
 // Configure the HTTP request pipeline.
@@ -175,3 +116,126 @@ app.MapControllers();
  
 app.Run();
  
+Get started with Swashbuckle and ASP.NET Core | Microsoft Learn
+Learn how to add Swashbuckle to your ASP.NET Core web API project to integrate the Swagger UI.
+ 
+2nd-Controllers/ArtWorkController.cs
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace dotnetapp.Controllers
+{
+    [ApiController]
+    [Route("api/artworks")]
+    public class ArtworkController : ControllerBase
+    {
+   public ApplicationDbContext db;
+      public ArtworkController(ApplicationDbContext db)
+      { this.db=db;
+       
+      }
+      [HttpGet]
+      public ActionResult<IEnumerable<Artwork>>GetArtworks(){
+        return Ok(db.Artworks.ToList());
+      }
+      [HttpGet("{id}")]
+      public ActionResult<Artwork>GetArtworkById(int id){
+        Artwork work=db.Artworks.FirstOrDefault(a=>a.ArtworkId==id);
+        if(work==null){
+          return NotFound();
+        }
+        return Ok(work);
+      }
+      [HttpGet("filter")]
+      public ActionResult<IEnumerable<Artwork>>GetArtworksByArtist([FromQuery] string artist)
+      {
+           List<Artwork> work=db.Artworks.Where(a=>a.Artist==artist).ToList();
+        if(work==null){
+          return NotFound();
+        }
+        return Ok(work);
+      }
+      [HttpPost]
+      public ActionResult<Artwork>CreateArtwork(Artwork work){
+        db.Artworks.Add(work);
+        db.SaveChanges();
+        return CreatedAtAction("CreateArtwork",work);
+      }
+    }
+}
+ 
+Models/ApplicationDbContext
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace dotnetapp.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Artwork>Artworks{get;set;}
+ 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options){
+ 
+        }
+       
+    }
+}
+ 
+Models/Artwork.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+namespace dotnetapp.Models
+{
+    public class Artwork
+    {
+        public int ArtworkId{get;set;}
+        public string Title{get;set;}
+        public string Artist{get;set;}
+        public int Year{get;set;}
+        public string Medium{get;set;}
+        public string Description{get;set;}
+    }
+}
+ 
+Program.cs
+using dotnetapp.Models;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+ 
+// Add services to the container.
+var conn="User ID=sa;password=examlyMssql@123;server=localhost;Database=appdb;trusted_connection=false;Persist Security Info=False;Encrypt=False";
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at Get started with Swashbuckle and ASP.NET Core | Microsoft Learn
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(db=>db.UseSqlServer(conn));
+var app = builder.Build();
+ 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+ 
+app.UseHttpsRedirection();
+ 
+app.UseAuthorization();
+ 
+app.MapControllers();
+ 
+app.Run();
+ 
+get; set
+Get started with Swashbuckle and ASP.NET Core
+Learn how to add Swashbuckle to your ASP.NET Core web API project to integrate the Swagger UI.
